9264a78581783577ebafe5704a10c252639b5098
Agus Lopez <contact@aguslr.com>
2016-10-31 13:18:29 +0100

    Ouput errors

--- makeUSB.sh
+++ makeUSB.sh
@@ -118,30 +118,27 @@ fi
 
 # Create GUID Partition Table
 printf 'Creating GUID Partition Table on %s... ' "$usb_dev"
-if sgdisk --mbrtogpt "$usb_dev" >/dev/null 2>&1; then
+if sgdisk --mbrtogpt "$usb_dev" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
 # Create BIOS boot partition (1M)
 printf 'Creating BIOS boot partition on %s... ' "$usb_dev"
 if sgdisk --new 1::+1M --typecode 1:ef02 \
-    --change-name 1:"BIOS boot partition" "$usb_dev" >/dev/null 2>&1; then
+    --change-name 1:"BIOS boot partition" "$usb_dev" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
 # Create EFI System partition (50M)
 printf 'Creating EFI System partition on %s... ' "$usb_dev"
 if sgdisk --new 2::+50M --typecode 2:ef00 --change-name 2:"EFI System" \
-    "$usb_dev" >/dev/null 2>&1; then
+    "$usb_dev" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -163,10 +160,9 @@ case "$data_fmt" in
 		;;
 esac
 if sgdisk --new 3::: --typecode 3:"$type_code" --change-name 3:"$part_name" \
-    "$usb_dev" >/dev/null 2>&1; then
+    "$usb_dev" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -176,10 +172,9 @@ umount --force ${usb_dev}* 2>/dev/null
 # Create hybrid MBR
 if [ "$interactive" -eq 0 ]; then
 	printf 'Creating hybrid MBR on %s... ' "${usb_dev}"
-	if sgdisk --hybrid 1:2:3 "$usb_dev" >/dev/null 2>&1; then
+	if sgdisk --hybrid 1:2:3 "$usb_dev" >/dev/null; then
 		printf 'OK\n'
 	else
-		printf 'FAILED\n'
 		cleanUp 10
 	fi
 else
@@ -190,10 +185,9 @@ fi
 
 # Set bootable flag for data partion
 printf 'Setting bootable flag on %s... ' "${usb_dev}3"
-if sgdisk --attributes 3:set:2 "$usb_dev" >/dev/null 2>&1; then
+if sgdisk --attributes 3:set:2 "$usb_dev" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -202,28 +196,25 @@ umount --force ${usb_dev}* 2>/dev/null
 
 # Format BIOS boot partition
 printf 'Formatting BIOS boot partition on %s... ' "${usb_dev}1"
-if dd if=/dev/zero of=${usb_dev}1 bs=512 count=1 >/dev/null 2>&1; then
+if dd if=/dev/zero of=${usb_dev}1 bs=512 count=1 >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
 # Format EFI System partition
 printf 'Formatting EFI System partition on %s... ' "${usb_dev}2"
-if mkfs.vfat -F 32 "${usb_dev}2" >/dev/null 2>&1; then
+if mkfs.vfat -F 32 "${usb_dev}2" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
 # Format data partition
 printf 'Formatting data partition as %s on %s... ' "$data_fmt" "${usb_dev}3"
-if mkfs.${data_fmt} "${usb_dev}3" >/dev/null 2>&1; then
+if mkfs.${data_fmt} "${usb_dev}3" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -235,7 +226,6 @@ printf 'Mounting EFI System partition on %s... ' "$efi_mnt"
 if mkdir -p "$efi_mnt" && mount "${usb_dev}2" "$efi_mnt"; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -244,17 +234,15 @@ printf 'Mounting data partition on %s... ' "$data_mnt"
 if mkdir -p "$data_mnt" && mount "${usb_dev}3" "$data_mnt"; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
 # Install GRUB for EFI
 printf 'Installing GRUB for EFI on %s... ' "$usb_dev"
 if grub-install --target=x86_64-efi --efi-directory=$efi_mnt \
-    --boot-directory=${data_mnt}boot --removable --recheck >/dev/null 2>&1; then
+    --boot-directory=${data_mnt}boot --removable --recheck >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -263,17 +251,15 @@ printf 'Unmounting EFI System partition on %s... ' "$efi_mnt"
 if umount $efi_mnt; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
 # Install GRUB for BIOS
 printf 'Installing GRUB for BIOS on %s... ' "$usb_dev"
 if grub-install --target=i386-pc --boot-directory=${data_mnt}boot \
-    --recheck "$usb_dev" >/dev/null 2>&1; then
+    --recheck "$usb_dev" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -281,10 +267,9 @@ fi
 printf 'Installing fallback GRUB on %s... ' "${usb_dev}3"
 if grub-install --force --target=i386-pc \
     --boot-directory=${data_mnt}boot \
-    --recheck "${usb_dev}3" >/dev/null 2>&1; then
+    --recheck "${usb_dev}3" >/dev/null; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -294,7 +279,6 @@ if mkdir -p ${data_mnt}boot/grub ${data_mnt}boot/bin ${data_mnt}boot/krnl \
     ${data_mnt}boot/isos; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -303,7 +287,6 @@ printf 'Copying files to %s... ' "${data_mnt}boot"
 if cp -rf ./grub.cfg ./grub.d ./multiboot.* "${data_mnt}boot/grub/"; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
@@ -312,7 +295,6 @@ printf 'Unmounting data partition on %s... ' "$data_mnt"
 if umount $data_mnt; then
 	printf 'OK\n'
 else
-	printf 'FAILED\n'
 	cleanUp 10
 fi
 
